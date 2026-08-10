52f1332433c977c4e788dd25261a7c244f94dcab
iznogoud <iznogoud@Anhs-Macbook-Pro.local>
2026-08-10 14:10:30 +0700

    fix: align desktop Nix dependency fetcher

--- nix/desktop-package.nix
+++ nix/desktop-package.nix
@@ -59,6 +59,7 @@ buildNpmPackage {
 
   nodejs = nodejs_22;
   inherit (paseo) npmDeps;
+  npmDepsFetcherVersion = 2;
 
   # Prevent onnxruntime-node's install script from running during automatic
   # npm rebuild. We manually rebuild only node-pty in buildPhase.
